10cea813b8b16076ad2b327fa3bb2c5f123d351e
dukesg@fwginc.com <dukesg@fwginc.com>
2018-09-13 10:42:09 -0700

    initial creation

---
+++ PasswordHub/Controllers/GenerationController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PasswordHub.Controllers
+{
+    public class GenerationController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewData["Message"] = "Generate a password";
+            return View();
+        }
+    }
+}
