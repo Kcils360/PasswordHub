7e77521b6975a26454b05a69d6a790760863185b
dukesg@fwginc.com <dukesg@fwginc.com>
2018-09-14 11:29:44 -0700

    maybe added the passwords tabe via migration?

--- PasswordHub/Data/Migrations/20180914182617_addedTable.Designer.cs
+++ PasswordHub/Data/Migrations/20180914182617_addedTable.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace PasswordHub.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180914182617_addedTable")]
+    partial class addedTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ PasswordHub/Models/Passwords.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PasswordHub.Models
+{
+    public class Passwords
+    {
+        [Key]        
+        public string ID { get; set; }
+        public string salt { get; set; }
+        public string hashedPword { get; set; }
+
+    }
+}
